141c585a2176e919d7c5f092e17a925f24fad0d4
wahabyounas <wahabyounas1234@gmail.com>
2021-10-13 12:04:12 +0500

    abc

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,274 +1,20 @@
-!(function($) {
+!(function ($) {
 	"use strict";
-// Pre-loader
-setTimeout(function(){
-	$('.loader-wrap').fadeToggle();
-  }, 2000);
+	// Pre-loader
+	setTimeout(function () {
+		$('.loader-wrap').fadeToggle();
+	}, 2000);
 
-// venobox
-$(document).ready(function(){
-	$('.venobox').venobox(); 
-});
-$('.counter-count').each(function () {
-	$(this).prop('counter',0).animate({
-		Counter: $(this).text()
-	}, {
-	  
-	  //chnage count up speed here
-		duration: 4000,
-		easing: 'swing',
-		step: function (now) {
-			$(this).text(Math.ceil(now));
-		}
+	// venobox
+	$(document).ready(function () {
+		$('.venobox').venobox();
 	});
-});
-     //contact validation
-window.addEventListener('load', function() {
-	// Fetch all the forms we want to apply custom Bootstrap validation styles to
-	var forms = document.getElementsByClassName('needs-validation');
-	// Loop over them and prevent submission
-	var validation = Array.prototype.filter.call(forms, function(form) {
-	  form.addEventListener('submit', function(event) {
-		if (form.checkValidity() === false) {
-		  event.preventDefault();
-		  event.stopPropagation();
-		}
-		form.classList.add('was-validated');
-	  }, false);
-	});
-  }, false);
-  /*===========================
-  == Contact Submittion Alert ==
-  =============================*/
-    window.addEventListener(
-      "load",
-      function () {
-        var forms = document.getElementsByClassName("needs-validation");
-        var validation = Array.prototype.filter.call(forms, function (form) {
-          form.addEventListener(
-            "submit",
-            function (event) {
-              if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-              }
-              if (form.checkValidity() === true) {
-                alert("Message has been Sent");
-              }
-              form.classList.add("was-validated");
-            },
-            false
-          );
-        });
-      },false);
-// Our-Schedule-corusel
-$('#schedule-carousel').owlCarousel({
-	    loop:true,
-	    nav:false,
-		dots:false,
-		autoplay:true,
-		smartSpeed: 2000,
-		animateOut: 'fadeOut',
-		// animateOut: 'slideOutDown',
-        animateIn: 'flipInX',
-	    autoplayHoverPause:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:1
-	        },
-	        1000:{
-	            items:1
-	        }
-	    }
-	})
-// Testimonial-carousel
-$('#trainers-carousel').owlCarousel({
-	center: true,
-	loop: true,
-	dots: true,
-	margin: 0,
-	responsive: {
-		0: {
-			items: 2,
-			margin: 10
-		},
-		576: {
-			items: 3
-		},
-		768: {
-			items: 4
-		},
-		1200: {
-			items: 6
-		}
-	}
-});
-	$('#testimonials-carousel').owlCarousel({
-	    loop:true,
-	    nav:false,
-		dots:false,
-		autoplay:true,
-		smartSpeed: 3000,
-	    autoplayHoverPause:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:1
-	        },
-	        1000:{
-	            items:1
-	        }
-	    }
-	})
-// Clients-carousel
-	$('#clients-carousel').owlCarousel({
-	    loop:true,
-	    margin:5,
-	    nav:false,
-		dots:false,
-		autoplay:false,
-		smartSpeed: 1500,
-	    autoplayTimeout:4000,
-	    autoplayHoverPause:true,
-	    responsive:{
-	        0:{
-	            items:3
-	        },
-	        600:{
-	            items:4
-	        },
-	        1000:{
-	            items:6
-	        },
-			1200:{
-	            items:6
-	        }
-	    }
-	})
-// design-carousel
-	$('#designs-carousel').owlCarousel({
-	    loop:true,
-	    margin:20,
-		autoplay:false,
-	    nav:false,
-		dots:false,
-	    autoplayHoverPause:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        600:{
-	            items:2
-	        },
-	        800:{
-	            items:3
-	        },
-			1000:{
-	            items:4
-	        },
-			1200:{
-				items:5
-			}
-	    }
-	})
-// owl-carousel
-	$('.owl-carousel').owlCarousel({
-		loop:true,
-		margin:10,
-		nav:false,
-		dots: false,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		responsive:{
-			0:{
-				items:1
-			},
-			600:{
-				items:1
-			},
-			1000:{
-				items:1
-			}
-		}
-	})
-//calculate-bmi
-window.onload = () => {
-	let button = document.querySelector("#btn");
-
-	// Function for calculating BMI
-	button.addEventListener("click", calculateBMI);
-};
-
-function calculateBMI() {
-
-	/* Getting input from user into height variable.
-	Input is string so typecasting is necessary. */
-	let height = parseInt(document
-			.querySelector("#height").value);
-
-	/* Getting input from user into weight variable.
-	Input is string so typecasting is necessary.*/
-	let weight = parseInt(document
-			.querySelector("#weight").value);
-
-	let result = document.querySelector("#result");
-
-	// Checking the user providing a proper
-	// value or not
-	if (height === "" || isNaN(height))
-		result.innerHTML = "Provide a valid Height!";
-
-	else if (weight === "" || isNaN(weight))
-		result.innerHTML = "Provide a valid Weight!";
-
-	// If both input is valid, calculate the bmi
-	else {
-
-		// Fixing upto 2 decimal places
-		let bmi = (weight / ((height * height)
-							/ 10000)).toFixed(2);
-
-		// Dividing as per the bmi conditions
-		if (bmi < 18.6) result.innerHTML =
-			` BMI: <span>${bmi}</span>`;
-
-		else if (bmi >= 18.6 && bmi < 24.9)
-			result.innerHTML =
-				`BMI: <span>${bmi}</span>`;
-
-		else result.innerHTML =
-			`BMI: <span>${bmi}</span>`;
-	}
-}
-
-// Filter-Gallery
-	$(document).ready(function(){
-		$('.btn-filter').click(function(){
-		  const value = $(this).attr('data-filter');
-		  if (value == 'all'){
-			$('.projects-item').show('1000');
-		  }
-		  else {
-			$('.projects-item').not('.'+value).hide('1000');
-			$('.projects-item').filter('.'+value).show('1000');
-		  }
-		})
-	  //add active class on selected item
-	  $('.btn-filter').click(function(){
-		$(this).addClass('btn-active').siblings().removeClass('btn-active');    
-		})
-	  })
-	  $('.counter-count').each(function () {
-		$(this).prop('Counter',0).animate({
+	$('.counter-count').each(function () {
+		$(this).prop('counter', 0).animate({
 			Counter: $(this).text()
 		}, {
-		  
-		  //chnage count up speed here
+
+			//chnage count up speed here
 			duration: 4000,
 			easing: 'swing',
 			step: function (now) {
@@ -276,37 +22,291 @@ function calculateBMI() {
 			}
 		});
 	});
-	window.onscroll=function(){
+	//contact validation
+	window.addEventListener('load', function () {
+		// Fetch all the forms we want to apply custom Bootstrap validation styles to
+		var forms = document.getElementsByClassName('needs-validation');
+		// Loop over them and prevent submission
+		var validation = Array.prototype.filter.call(forms, function (form) {
+			form.addEventListener('submit', function (event) {
+				if (form.checkValidity() === false) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+				form.classList.add('was-validated');
+			}, false);
+		});
+	}, false);
+	/*===========================
+	== Contact Submittion Alert ==
+	=============================*/
+	window.addEventListener(
+		"load",
+		function () {
+			var forms = document.getElementsByClassName("needs-validation");
+			var validation = Array.prototype.filter.call(forms, function (form) {
+				form.addEventListener(
+					"submit",
+					function (event) {
+						if (form.checkValidity() === false) {
+							event.preventDefault();
+							event.stopPropagation();
+						}
+						if (form.checkValidity() === true) {
+							alert("Message has been Sent");
+						}
+						form.classList.add("was-validated");
+					},
+					false
+				);
+			});
+		}, false);
+	// Our-Schedule-corusel
+	$('#schedule-carousel').owlCarousel({
+		loop: true,
+		nav: false,
+		dots: false,
+		autoplay: true,
+		smartSpeed: 2000,
+		animateOut: 'fadeOut',
+		// animateOut: 'slideOutDown',
+		animateIn: 'flipInX',
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 1
+			}
+		}
+	})
+	// Testimonial-carousel
+	$('#trainers-carousel').owlCarousel({
+		center: true,
+		loop: true,
+		dots: true,
+		margin: 0,
+		responsive: {
+			0: {
+				items: 2,
+				margin: 10
+			},
+			576: {
+				items: 3
+			},
+			768: {
+				items: 4
+			},
+			1200: {
+				items: 6
+			}
+		}
+	});
+	$('#testimonials-carousel').owlCarousel({
+		loop: true,
+		nav: false,
+		dots: false,
+		autoplay: true,
+		smartSpeed: 3000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 1
+			}
+		}
+	})
+	// Clients-carousel
+	$('#clients-carousel').owlCarousel({
+		loop: true,
+		margin: 5,
+		nav: false,
+		dots: false,
+		autoplay: false,
+		smartSpeed: 1500,
+		autoplayTimeout: 4000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 3
+			},
+			600: {
+				items: 4
+			},
+			1000: {
+				items: 6
+			},
+			1200: {
+				items: 6
+			}
+		}
+	})
+	// design-carousel
+	$('#designs-carousel').owlCarousel({
+		loop: true,
+		margin: 20,
+		autoplay: false,
+		nav: false,
+		dots: false,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 2
+			},
+			800: {
+				items: 3
+			},
+			1000: {
+				items: 4
+			},
+			1200: {
+				items: 5
+			}
+		}
+	})
+	// owl-carousel
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: false,
+		dots: false,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 1
+			}
+		}
+	})
+	//calculate-bmi
+	window.onload = () => {
+		let button = document.querySelector("#btn");
+
+		// Function for calculating BMI
+		button.addEventListener("click", calculateBMI);
+	};
+
+	function calculateBMI() {
+
+		/* Getting input from user into height variable.
+		Input is string so typecasting is necessary. */
+		let height = parseInt(document
+			.querySelector("#height").value);
+
+		/* Getting input from user into weight variable.
+		Input is string so typecasting is necessary.*/
+		let weight = parseInt(document
+			.querySelector("#weight").value);
+
+		let result = document.querySelector("#result");
+
+		// Checking the user providing a proper
+		// value or not
+		if (height === "" || isNaN(height))
+			result.innerHTML = "Provide a valid Height!";
+
+		else if (weight === "" || isNaN(weight))
+			result.innerHTML = "Provide a valid Weight!";
+
+		// If both input is valid, calculate the bmi
+		else {
+
+			// Fixing upto 2 decimal places
+			let bmi = (weight / ((height * height)
+				/ 10000)).toFixed(2);
+
+			// Dividing as per the bmi conditions
+			if (bmi < 18.6) result.innerHTML =
+				` BMI: <span>${bmi}</span>`;
+
+			else if (bmi >= 18.6 && bmi < 24.9)
+				result.innerHTML =
+					`BMI: <span>${bmi}</span>`;
+
+			else result.innerHTML =
+				`BMI: <span>${bmi}</span>`;
+		}
+	}
+
+	// Filter-Gallery
+	$(document).ready(function () {
+		$('.btn-filter').click(function () {
+			const value = $(this).attr('data-filter');
+			if (value == 'all') {
+				$('.projects-item').show('1000');
+			}
+			else {
+				$('.projects-item').not('.' + value).hide('1000');
+				$('.projects-item').filter('.' + value).show('1000');
+			}
+		})
+		//add active class on selected item
+		$('.btn-filter').click(function () {
+			$(this).addClass('btn-active').siblings().removeClass('btn-active');
+		})
+	})
+	$('.counter-count').each(function () {
+		$(this).prop('Counter', 0).animate({
+			Counter: $(this).text()
+		}, {
+
+			//chnage count up speed here
+			duration: 4000,
+			easing: 'swing',
+			step: function (now) {
+				$(this).text(Math.ceil(now));
+			}
+		});
+	});
+	window.onscroll = function () {
 		myfunction()
 	};
 	var navbar = document.getElementById("mynavbar");
-	function myfunction(){
-		if(window.pageYOffset>=100){
+	function myfunction() {
+		if (window.pageYOffset >= 100) {
 			navbar.classList.add("sticky")
 		}
-		else{
+		else {
 			navbar.classList.remove("sticky");
 		}
 	}
-/*===========================
-====== Porfolio Filter ======
-=============================*/
-$(document).ready(function () {
-    $(".filter-button").click(function () {
-      var value = $(this).attr('data-filter');
-      if (value == "all") {
-        $('.filter').show('1000');
-      }
-      else {
-        $(".filter").not('.' + value).hide('3000');
-        $('.filter').filter('.' + value).show('3000');
-      }
-  // active
-      $(".filter-button").removeClass('active');
-      var $this = $(this);
-      if (!$this.hasClass('active')) {
-        $this.addClass('active');
-      }
-    });
-  });
+	/*===========================
+	====== Porfolio Filter ======
+	=============================*/
+	$(document).ready(function () {
+		$(".filter-button").click(function () {
+			var value = $(this).attr('data-filter');
+			if (value == "all") {
+				$('.filter').show('1000');
+			}
+			else {
+				$(".filter").not('.' + value).hide('3000');
+				$('.filter').filter('.' + value).show('3000');
+			}
+			// active
+			$(".filter-button").removeClass('active');
+			var $this = $(this);
+			if (!$this.hasClass('active')) {
+				$this.addClass('active');
+			}
+		});
+	});
 })(jQuery);
